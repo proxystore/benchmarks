89509f93d9cbd4f9db49626618b2d29c976b324c
Greg Pauloski <18683347+gpauloski@users.noreply.github.com>
2024-03-01 22:51:56 -0600

    Unregister store in colmena benchmark

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,22 @@
 from __future__ import annotations
 
+from typing import Generator
+
+import proxystore
+import pytest
+
 from testing.fixtures import file_store
 from testing.fixtures import local_store
 from testing.fixtures import process_executor
 from testing.fixtures import thread_executor
+
+
+@pytest.fixture(autouse=True)
+def _verify_no_registered_stores() -> Generator[None, None, None]:
+    yield
+
+    if len(proxystore.store._stores) > 0:  # pragma: no cover
+        raise RuntimeError(
+            'Test left at least one store registered: '
+            f'{tuple(proxystore.store._stores.keys())}.',
+        )
